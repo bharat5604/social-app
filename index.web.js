6fd3dde16a60f1ea204de8ec65a03cc3ba90d675
Bharat Singh Rajput <46484617+bharat5604@users.noreply.github.com>
2023-05-24 22:23:03 +0530

    Update index.web.js

--- index.web.js
+++ index.web.js
@@ -1,4 +1,25 @@
-import 'platform/polyfills'
-import {registerRootComponent} from 'expo'
-import App from './src/App'
-registerRootComponent(App)
+{
+  "workbench.iconTheme": "vscode-icons",
+  "typescript.suggest.paths": false,
+  "javascript.suggest.paths": false,
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "prettier.documentSelectors": ["**/*.astro"],
+  "[astro]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "editor.formatOnPaste": true,
+  "editor.formatOnSave": true,
+  "editor.minimap.enabled": false,
+  "explorer.confirmDelete": false,
+  "window.zoomLevel": 1,
+  "tailwindCSS.emmetCompletions": true,
+  "editor.inlineSuggest.enabled": true,
+  "editor.quickSuggestions": {
+    "strings": true
+  },
+  "css.validate": false,
+  "tailwindCSS.includeLanguages": {
+    "plaintext": "html"
+  },
+  "tabnine.experimentalAutoImports": true
+}
